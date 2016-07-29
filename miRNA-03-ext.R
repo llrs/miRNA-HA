84017a0461323978e9669e2825c6c5b0544de7f9
Lluís Revilla <lluis.revilla@gmail.com>
2016-07-29 12:04:12 +0200

    Saving results of WGCNA

--- miRNA-03-ext.R
+++ miRNA-03-ext.R
@@ -214,22 +214,62 @@ GGMMfun <- function(x, var, MM, GS, GSP, MMP, moduleColors, modNames,
   
   # Weights of the correlation
   w <- (1 - data[,"GSP"]) * (1 - data[,"MMP"])
+  # Calculates the weighted mean of genes correlation with the trait
+  wgenecor <- weighted.mean(data[,"GS"], (1 - data[,"GSP"]), na.rm = TRUE)
+  wmmcor <- weighted.mean(data[,"MM"], (1 - data[,"MMP"]), na.rm = TRUE)
+  
   # Taking into account if there are empty values
-  svar <- apply(data[,c("MM", "GS")], 1, function(x){sum(is.na(x))})
-  w.cor <- corr(data[!as.logical(svar), c("MM", "GS")], w[!as.logical(svar)])
+  gene <- !as.logical(apply(data[,c("MM", "GS")], 1, 
+                            function(x){sum(is.na(x))}))
+  w.cor <- corr(data[gene, c("MM", "GS")], w[gene])
+  u.cor <- cor(x = data[gene, "MM"], y = data[gene, "GS"])
+  
   png(file = paste("MM_GS", var, module, ".png", sep = "_"), 
        width = 700, height = 700)
-  verboseScatterplot(data[!as.logical(svar), "MM"], 
-                     data[!as.logical(svar), "GS"], 
+  verboseScatterplot(data[gene, "MM"], data[gene, "GS"], 
                      xlab = paste("Module Membership in", module, "module"), 
                      ylab = paste("Gene significance for", var), 
-                     main = paste0("Module membership vs. gene significance\nWeighted cor=", 
+                     main = paste0(
+                       "Module membership vs. gene significance\nWeighted cor=", 
                                    signif(w.cor, digits = 2), ", unweighted"),
                      abline = 1, 
                      cex.main = 1.2, cex.lab = 1.2, cex.axis = 1.2, 
                      col = ifelse(module %in% c("white", "floralwhite"),
-                                  "black", module))
+                                  "black", module),
+                     sub = paste(
+                       "Correlation of genes with trait: Weighted mean", 
+                                 signif(wgenecor, 2), "normal", 
+                                 signif(mean(data[, "GS"], na.rm = TRUE), 2)))
   dev.off()
+  
+  png(file = paste("MM_GS_ggplot", var, module, ".png", sep = "_"), 
+      width = 700, height = 700)
+  # With ggplot with size for the weights
+  ab <- lm(data[gene, "GS"] ~ data[gene, "MM"])
+  plot.g <- ggplot() +
+    geom_point(aes(x = data[gene, "MM"], y = data[gene, "GS"], 
+                   size = w[gene]),
+               col = ifelse(module %in% c("white", "floralwhite"), 
+                            "black", module)) + theme_bw() + 
+    geom_abline(slope = ab$coefficients[2], intercept = ab$coefficients[1]) +
+    labs(title = paste0("Module membership vs. gene significance",
+                        "\nWeighted cor=", signif(w.cor, digits = 2), 
+                        ", unweighted cor=", signif(u.cor, digits = 2), 
+                        " p=", 
+                        signif(corPvalueStudent(u.cor, nrow(data[gene, ])), 
+                               digits = 2))) +
+    xlab(paste("Module Membership in", module, "module")) +
+    ylab(paste("Gene significance for", var))
+  # Point of the weighted mean
+  # geom_point(aes(wmmcor, wgenecor),
+  #            col = ifelse(module == "red", "green", "red")) +
+  # Point of the unweighted mean
+  # geom_point(aes(mean(data[gene, "MM"], na.rm = TRUE),
+  #                mean(data[gene, "GS"]), na.rm = TRUE),
+  #            col = ifelse(module == "green", "orange", "green"))
+  
+  ggsave(filename = paste("MM_GS", var, module, "ggplot.png", sep = "_"),
+         plot = plot.g)
 }
 
 # GGMMfun("salmon", "status_90", geneModuleMembership, geneTraitSignificance,
@@ -245,59 +285,55 @@ a <- sapply(names(IM), function(y, d){
          modNames = modNames, disease = disease)
 }, d = IM)
 
-# ==============================================================================
-#
-#  Code chunk 8: Annotate the probes with a gene name
-#
-# ==============================================================================
-
-#
-# annot = read.csv(file = "GeneAnnotation.csv") 
-# dim(annot)
-# names(annot)
-# probes = names(data.wgcna)
-# probes2annot = match(probes, annot$substanceBXH)
-# # The following is the number or probes without annotation:
-# sum(is.na(probes2annot))
-# # Should return 0.
-
 
 # ==============================================================================
 #
-#  Code chunk 9
+#  Code chunk 8: Store the results of the WGCNA
 #
 # ==============================================================================
 
 
-# Create the starting data frame
-# geneInfo0 <- data.frame(substanceBXH = probes, 
-#                       geneSymbol = annot$gene_symbol[probes2annot], 
-#                       LocusLinkID = annot$LocusLinkID[probes2annot], 
-#                       moduleColor = moduleColors, 
-#                       geneTraitSignificance, 
-#                       GSPvalue)
-# # Order modules by their significance for weight
-# modOrder <- order(-abs(cor(MEs, weight, use = "p"))) 
-# # Add module membership information in the chosen order
-# for (mod in 1:ncol(geneModuleMembership)) {
-#   oldNames = names(geneInfo0)
-#   geneInfo0 <- data.frame(geneInfo0, geneModuleMembership[, modOrder[mod]], 
-#                          MMPvalue[, modOrder[mod]]) 
-#   names(geneInfo0)<- c(oldNames, paste0("MM.", modNames[modOrder[mod]]), 
-#                        paste0("p.MM.", modNames[modOrder[mod]]))
-# }
-# # Order the genes in the geneInfo variable first by module color, 
-# # then by geneTraitSignificance
-# geneOrder <- order(geneInfo0$moduleColor, -abs(geneInfo0$GS.weight)) 
-# geneInfo <- geneInfo0[geneOrder, ]
-#
-#
-# ==============================================================================
-# 
-#  Code chunk 10: Save information about each gene in a csv file
-# 
-# ==============================================================================
-#
-#
-# write.csv(geneInfo, file = "geneInfo.csv")
+# Create a dataframe with gene symbol, modul, MM and MMPvalue
+geneInfo <- data.frame(miRNA = colnames(data.wgcna),
+                       moduleColor = moduleColors)
+# Append all the data of MM and MMP
+geneInfo0 <- merge(geneInfo, geneModuleMembership, by.x = "miRNA",
+                   by.y = 0, all = TRUE)
+geneInfo0 <- merge(geneInfo0, MMPvalue, by.x = "miRNA",
+                   by.y = 0, all = TRUE)
 
+# Extract them for the specific probes
+geneInfo1 <- lapply(unique(geneInfo$moduleColor), function(x, gI){
+  mm.positions <- grep(paste0("MM", x), colnames(gI))
+  info.re <- gI[gI$moduleColor == x, c(1, mm.positions)]
+  MM <- data.frame("miRNA" = info.re[, 1], "MM" = info.re[, 2])
+  MMP <- data.frame("miRNA" = info.re[, 1], "MMP.value" = info.re[, 3])
+  return(list(unique(MM), unique(MMP)))
+}, gI = geneInfo0)
+
+# Concatenate them
+MM <- data.frame()
+MMP <- data.frame()
+for (i in 1:length(geneInfo1)) {
+  MM <- rbind(MM, geneInfo1[[i]][[1]])
+  MMP <- rbind(MMP, geneInfo1[[i]][[2]])
+}
+# Add them to the original information
+geneInfo2 <- merge(geneInfo, MM, by = "miRNA", all.x = TRUE)
+geneInfo2 <- merge(geneInfo2, MMP, by = "miRNA", all.x = TRUE)
+write.csv(geneInfo2, "genes_modules.csv", row.names = FALSE, na = "")
+
+
+# Foreach module create a table in a file with genes, GS GS-P.values
+geneInfo1 <- lapply(
+  unique(geneInfo$moduleColor), 
+  function(x, gI, GS, GSP, d, ...){
+    gT <- gI[gI$moduleColor == x, ]
+    gT <- merge(gT, GS, by.x = "miRNA", by.y = 0, all.x = TRUE, all.y = FALSE)
+    gT <- merge(gT, GSP, by.x = "miRNA", by.y = 0, all.x = TRUE, all.y = FALSE)
+    ord <- sapply(colnames(d), function(x){grep(x, colnames(gT))})
+    ord <- c(1, 2, 3, unique(unlist(ord)))
+    gT <- gT[, ord]
+    write.csv(gT, paste(x, "trait.csv", sep = "_"), row.names = FALSE, na = "")
+  }, 
+  gI = geneInfo, GS = geneTraitSignificance, GSP = GSPvalue, d = disease)
